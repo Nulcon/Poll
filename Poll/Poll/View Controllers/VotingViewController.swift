aac101ef282efb9683d6968045a3e55d75745948
Conner Alegre <cga0@pm.me>
2018-07-26 13:34:37 -0600

    add Voting storyboard controller with relevant outlets

---
+++ Poll/Poll/View Controllers/VotingViewController.swift
@@ -0,0 +1,33 @@
+//
+//  VotingViewController.swift
+//  Poll
+//
+//  Created by Conner on 7/26/18.
+//  Copyright © 2018 Conner. All rights reserved.
+//
+
+import UIKit
+
+class VotingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    @IBAction func submitVote(_ sender: Any) {
+        
+    }
+    
+    // MARK: - Properties
+    @IBOutlet var nameTextField: UITextField!
+    @IBOutlet var responseTextField: UITextField!
+}
